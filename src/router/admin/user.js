4c94ec342e4c6fbbcb352b733cb9c0bceb95b927
unikZer0 <unik09john@gmail.com>
2025-05-11 19:09:41 +0700

    k

--- src/router/admin/user.js
+++ src/router/admin/user.js
@@ -4,6 +4,6 @@ const getUsers = require('../../adminCtrl/user/UserCtrl')
 const verifyToken = require('../../controller/tokenhandle/verifyToken')
 const authorizeRole = require('../../middleware/authorizeRole')
 
-router.post('/getusers/',verifyToken,authorizeRole(1,2),getUsers.getAllUsersCtrl);
+router.post('/getusers/',getUsers.getAllUsersCtrl);
 
 module.exports = router
