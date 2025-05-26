d25a1091be96fcadb0415ab311fa03f13b55a390
BounHiengSomphong <teng.ios123@gmail.com>
2025-05-27 01:39:53 +0700

    a.Address_ID

--- src/controller/product/query/orderPageQuery.js
+++ src/controller/product/query/orderPageQuery.js
@@ -1,7 +1,7 @@
 //address
 const addressQueries = {
     insert:`INSERT INTO address (User_ID,Village,District,Province,Transportation,Branch) VALUES(?,?,?,?,?,?)`,
-    show:`SELECT u.FirstName ,u.LastName ,u.Phone,a.Transportation,a.Branch, a.Village, a.District ,a.Province FROM address a JOIN users u ON a.User_ID = u.User_ID WHERE u.User_ID = ?`
+    show:`SELECT a.Address_ID,u.FirstName ,u.LastName ,u.Phone,a.Transportation,a.Branch, a.Village, a.District ,a.Province FROM address a JOIN users u ON a.User_ID = u.User_ID WHERE u.User_ID = ?`
 }
 const orderQuery = {
     insertOrder:`INSERT INTO orders SET ?`,
