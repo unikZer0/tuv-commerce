1579c7a87524b216c6a08c0f5665f4c4ffca09f1
unikZer0 <unik09john@gmail.com>
2025-06-13 13:51:59 +0700

    ate loadpage

--- src/controller/product/query/categoryQuery.js
+++ src/controller/product/query/categoryQuery.js
@@ -2,39 +2,43 @@
 const cateQueries = {
      getProductsQuery : `
 SELECT 
-    p.Product_ID,
-    p.Name,
-    p.Description,
-    p.Brand,
-    p.Image,
-    p.Price,
-    GROUP_CONCAT(DISTINCT i.Size ORDER BY i.Size SEPARATOR ', ') AS Sizes,
-    GROUP_CONCAT(DISTINCT i.Color ORDER BY i.Color SEPARATOR ', ') AS Colors,
-    SUM(i.Quantity) AS Total_Quantity
-FROM 
-    products p
-JOIN 
-    inventory i ON p.Product_ID = i.Product_ID
-GROUP BY 
-    p.Product_ID;
+        p.Product_ID,
+        p.Name,
+        p.Description,
+        p.Brand,
+        p.Image,
+        p.Price,
+        GROUP_CONCAT(DISTINCT i.Size ORDER BY i.Size SEPARATOR ', ') AS Sizes,
+        GROUP_CONCAT(DISTINCT i.Color ORDER BY i.Color SEPARATOR ', ') AS Colors,
+        SUM(i.Quantity) AS Total_Quantity
+      FROM 
+        products p
+      JOIN 
+        inventory i ON p.Product_ID = i.Product_ID
+      GROUP BY 
+        p.Product_ID
+      LIMIT ? OFFSET ?
 `,
  getCateQuery :`
 SELECT 
-    p.Product_ID,
-    p.Name,
-    p.Description,
-    p.Brand,
-    p.Image,
-    p.Price,
-    GROUP_CONCAT(DISTINCT i.Size ORDER BY i.Size SEPARATOR ', ') AS Sizes,
-    GROUP_CONCAT(DISTINCT i.Color ORDER BY i.Color SEPARATOR ', ') AS Colors,
-    SUM(i.Quantity) AS Total_Quantity
-FROM 
-    products p
-JOIN 
-    inventory i ON p.Product_ID = i.Product_ID WHERE p.productType_ID = ?
-GROUP BY 
-    p.Product_ID;
+        p.Product_ID,
+        p.Name,
+        p.Description,
+        p.Brand,
+        p.Image,
+        p.Price,
+        GROUP_CONCAT(DISTINCT i.Size ORDER BY i.Size SEPARATOR ', ') AS Sizes,
+        GROUP_CONCAT(DISTINCT i.Color ORDER BY i.Color SEPARATOR ', ') AS Colors,
+        SUM(i.Quantity) AS Total_Quantity
+      FROM 
+        products p
+      JOIN 
+        inventory i ON p.Product_ID = i.Product_ID
+      WHERE 
+        p.productType_ID = ?
+      GROUP BY 
+        p.Product_ID
+      LIMIT ? OFFSET ?
     
 `
 }
